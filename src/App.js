28298d151413445c170d63d47415d40456825ecc
Subatta Patil <patilsubatta@gmail.com>
2025-12-22 21:10:55 +0530

    Initial commit - Property Listing App with filters and chips

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
 import About from "./pages/About";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/properties" element={<Properties />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/properties" />} />
+      <Route path="/properties" element={<Properties />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+
   );
 }
 
